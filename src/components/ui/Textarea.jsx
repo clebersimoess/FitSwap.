5dc4c990dd048fdf5b3bb355a1adfd53a3212b3e
clebersimoess <clebersimoessilva@gmail.com>
2025-11-19 14:09:59 -0300

    Create Textarea.jsx

---
+++ src/components/ui/Textarea.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Textarea = ({ 
+  className = '',
+  ...props 
+}) => {
+  return (
+    <textarea
+      className={`w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent ${className}`}
+      {...props}
+    />
+  );
+};
